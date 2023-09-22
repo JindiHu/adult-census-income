1f34cc82534c83d72b3b3e7b9f2c5ff20ac77a95
Hu Jindi <hujindi1212@gmail.com>
2023-09-22 23:43:54 +0800

    feat: random forest (#2)
    * chore: refactor heatmap
    
    * feat: decision tree and random forest
    
    ---------
    
    Co-authored-by: Hu Jindi <hu_jindi@tech.gov.sg>

--- constant.py
+++
@@ -1,6 +0,0 @@
-DATA_NAMES = ["age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation",
-              "relationship", "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country",
-              "income"]
-TARGET = "income"
-TRAIN_DATASET_FILEPATH = "./census_income/adult.data"
-TEST_DATASET_FIREPATH = "./census_income/adult.test"
--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 import sys
 
-import constant
-import utils
-from eda import perform_eda
+from sklearn.model_selection import train_test_split
 
-if __name__ == '__main__':
+import utils
+from decision_tree import DecisionTree
+from eda import perform_eda
+from random_forest import RandomForest
+
+col_names = ["age", "workclass", "fnlwgt", "education", "education-num", "marital-status",
+             "occupation", "relationship", "race", "sex", "capital-gain", "capital-loss",
+             "hours-per-week", "native-country", "income"]
+
+if __name__ == "__main__":
     if sys.version_info[0:2] != (3, 11):
-        raise Exception('Requires python 3.11')
+        raise Exception("Requires python 3.11")
 
     # load dataset into pandas dataframe
-    df = utils.load_data(constant.TRAIN_DATASET_FILEPATH, constant.DATA_NAMES)
+    df = utils.load_data("./census_income/adult.data", col_names)
 
     # perform Exploratory Data Analysis (EDA)
-    df = perform_eda(df)
+    x, y = perform_eda(df)
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=0)
+
+    dt = DecisionTree(features=x_train, target=y_train, max_depth=6)
+    print("training decision tree ...")
+    dt.fit()
+    print("evaluating decision tree ...")
+    dt.evaluate(x_test, y_test)
+
+    rf = RandomForest(features=x_train, target=y_train, n_estimators=6, sample_frac=0.8, max_depth=4)
+    print("training random forest ...")
+    rf.fit()
+    print("evaluating random forest ...")
+    rf.evaluate(x_test, y_test)
