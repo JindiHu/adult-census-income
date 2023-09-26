69ee785cf772b35b29577458dabbb396bea2ad62
Hu Jindi <hujindi1212@gmail.com>
2023-09-26 13:21:02 +0800

    chore: test model on test dataset (#4)
    * chore: test model on test dataset
    
    * chore: update performance metric plots
    
    ---------
    
    Co-authored-by: Hu Jindi <hu_jindi@tech.gov.sg>

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 
 def load_data(file_name: str, column_names: list[str]) -> pd.DataFrame:
@@ -8,3 +9,12 @@ def load_data(file_name: str, column_names: list[str]) -> pd.DataFrame:
     df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
 
     return df
+
+
+def encode_values(cols: list, train_df: pd.DataFrame, test_df: pd.DataFrame = None):
+    label_encoder = LabelEncoder()
+    for col in cols:
+        label_encoder.fit(train_df[col])
+        train_df[col] = label_encoder.transform(train_df[col])
+        if test_df is not None:
+            test_df[col] = label_encoder.transform(test_df[col])
